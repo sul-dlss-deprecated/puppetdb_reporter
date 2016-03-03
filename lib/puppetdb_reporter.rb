52c1630ea689ddd0a13e7a79b61c1c99d3f49433
Anthony Zanella <azanella@stanford.edu>
2016-03-02 16:46:04 -0800

    looking at hostnames and technical_teams

--- lib/puppetdb_reporter.rb
+++ lib/puppetdb_reporter.rb
@@ -1,2 +1,36 @@
+require 'puppetdb'
+require 'json'
+
 class PuppetdbReporter
+
+  attr_reader :endpoint
+  attr_reader :client
+
+  def initialize(endpoint)
+    @endpoint = endpoint
+    @client = PuppetDB::Client.new(server: @endpoint)
+  end
+
+  def fact_query(hostname)
+    response = @client.request('facts', ['=', 'name', 'hostname'])
+    #  my_hash = JSON.parse(response.data)
+    #  puts my_hash.class
+    puts response.data.class
+  end
+
+  def number_of_hostnames
+    response = @client.request('facts', ['=', 'name', 'hostname'])
+    response.data.count
+  end
+
+  def technical_team_count
+    response = @client.request('facts', ['=', 'name', 'technical_team'])
+    response.data.count
+  end
+
+  def hostnames
+    response = @client.request('facts', ['=', 'name', 'hostname'])
+    response.data.collect { |x| x['certname'] }
+  end
+
 end
--- spec/puppetdb_reporter_spec.rb
+++ spec/puppetdb_reporter_spec.rb
@@ -4,7 +4,36 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'puppetdb_reporter.rb'
 
 describe 'PuppetdbReporter' do
-  it 'can instantiate' do
-    expect(PuppetdbReporter.new).to be_kind_of(PuppetdbReporter)
+
+  before(:all) do
+    @puppetdb_reporter = PuppetdbReporter.new('http://sulpuppet-db.stanford.edu:8080')
   end
+
+  it 'can instantiate with an endpoint url' do
+    expect(@puppetdb_reporter).to be_kind_of(PuppetdbReporter)
+  end
+
+  it 'can return its endpoint url' do
+    expect(@puppetdb_reporter.endpoint).to eql('http://sulpuppet-db.stanford.edu:8080')
+  end
+
+  it 'has a PuppetDB::Client' do
+    expect(@puppetdb_reporter.client).to be_kind_of(PuppetDB::Client)
+  end
+
+  it 'can return the number of hostnames' do
+    expect(@puppetdb_reporter.number_of_hostnames).to eql(464)
+  end
+
+  #how many things have a technical_team
+  it 'can return the number of nodes with a technical_team' do
+    expect(@puppetdb_reporter.technical_team_count).to eql(114)
+  end
+
+  it 'return a list of hostnames' do
+    expect(@puppetdb_reporter.hostnames).to be_kind_of(Array)
+  end
+
+  #foreach hostname what is it's technical_team
+
 end
