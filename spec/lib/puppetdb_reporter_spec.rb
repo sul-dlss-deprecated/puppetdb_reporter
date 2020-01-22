833d371b512c5401618d3678ed94877c119a82e1
Tony Zanella <tony.zanella@gmail.com>
2020-01-22 10:20:52 -0800

    Consolidate spec responses

--- spec/lib/puppetdb_reporter_spec.rb
+++ spec/lib/puppetdb_reporter_spec.rb
@@ -5,6 +5,13 @@ describe 'PuppetdbReporter' do
 
   before(:all) do
     @puppetdb_reporter = PuppetdbReporter.new('http://sulpuppet-db.stanford.edu:8080')
+    @hostnames_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/hostname_facts')).read
+    @technical_teams_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/technical_team_facts')).read
+    @departments_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/department_facts')).read
+    @technical_team_for_hostname_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/technical_team_for_hostname')).read
+    @user_advocate_for_hostname_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/user_advocate_for_hostname')).read
+    @project_for_hostname_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/project_for_hostname')).read
+    @sla_for_hostname_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/sla_for_hostname')).read
   end
 
   it 'can instantiate with an endpoint url' do
@@ -20,89 +27,76 @@ describe 'PuppetdbReporter' do
   end
 
   it 'can return the number of hostnames' do
-    response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/hostname_facts')).read
     stub_request(:any, /sulpuppet-db/).to_return(status: 200,
-                                                 body: response,
+                                                 body: @hostnames_response,
                                                  headers: { 'Content-Type' => 'application/json' })
     expect(@puppetdb_reporter.number_of_hostnames).to eql(2)
   end
 
   it 'can return the number of nodes with a technical_team' do
-    response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/technical_team_facts')).read
     stub_request(:any, /sulpuppet-db/).to_return(status: 200,
-                                                 body: response,
+                                                 body: @technical_teams_response,
                                                  headers: { 'Content-Type' => 'application/json' })
     expect(@puppetdb_reporter.technical_team_count).to eql(2)
   end
 
   it 'returns a list of hostnames' do
-    response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/hostname_facts')).read
     stub_request(:any, /sulpuppet-db/).to_return(status: 200,
-                                                 body: response,
+                                                 body: @hostnames_response,
                                                  headers: { 'Content-Type' => 'application/json' })
     expect(@puppetdb_reporter.hostnames).to be_kind_of(Array)
   end
 
   it 'can return a department for a given hostname' do
-    response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/department_facts')).read
     stub_request(:any, /sulpuppet-db/).to_return(status: 200,
-                                                 body: response,
+                                                 body: @departments_response,
                                                  headers: { 'Content-Type' => 'application/json' })
     expect(@puppetdb_reporter.get_department('sul-frda-prod.stanford.edu')).to eql('dlss')
   end
 
   it 'can return a technical_team for a given hostname' do
-    response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/technical_team_for_hostname')).read
     stub_request(:any, /sulpuppet-db/).to_return(status: 200,
-                                                 body: response,
+                                                 body: @technical_team_for_hostname_response,
                                                  headers: { 'Content-Type' => 'application/json' })
     expect(@puppetdb_reporter.get_technical_team('some-server-prod.stanford.edu')).to eql('bar')
   end
 
   it 'can return a user_advocate for a given hostname' do
-    response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/user_advocate_for_hostname')).read
     stub_request(:any, /sulpuppet-db/).to_return(status: 200,
-                                                 body: response,
+                                                 body: @user_advocate_for_hostname_response,
                                                  headers: { 'Content-Type' => 'application/json' })
     expect(@puppetdb_reporter.get_user_advocate('some-server-prod.stanford.edu')).to eql('foo')
   end
 
   it 'can return a project for a given hostname' do
-    response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/project_for_hostname')).read
     stub_request(:any, /sulpuppet-db/).to_return(status: 200,
-                                                 body: response,
+                                                 body: @project_for_hostname_response,
                                                  headers: { 'Content-Type' => 'application/json' })
     expect(@puppetdb_reporter.get_project('some-server-prod.stanford.edu')).to eql('baz')
   end
 
   it 'can return an sla_level for a given hostname' do
-    response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/sla_for_hostname')).read
     stub_request(:any, /sulpuppet-db/).to_return(status: 200,
-                                                 body: response,
+                                                 body: @sla_for_hostname_response,
                                                  headers: { 'Content-Type' => 'application/json' })
     expect(@puppetdb_reporter.get_sla_level('some-server-prod.stanford.edu')).to eql('low')
   end
 
   it 'returns an array of facts for a hostname' do
-    departments_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/department_facts')).read
-    technical_teams_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/technical_team_facts')).read
-    user_advocates_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/user_advocate_for_hostname')).read
-    projects_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/project_for_hostname')).read
-    sla_levels_response = File.open(File.join(File.dirname(__FILE__), '../fixtures/files/sla_for_hostname')).read
     stub_request(:any, /sulpuppet-db/).to_return({ status: 200,
-                                                   body: departments_response,
+                                                   body: @departments_response,
                                                    headers: { 'Content-Type' => 'application/json' } },
                                                  { status: 200,
-                                                   body: technical_teams_response,
+                                                   body: @technical_teams_response,
                                                    headers: { 'Content-Type' => 'application/json' } },
                                                  { status: 200,
-                                                   body: user_advocates_response,
+                                                   body: @user_advocate_for_hostname_response,
                                                    headers: { 'Content-Type' => 'application/json' } },
                                                  { status: 200,
-                                                   body: projects_response,
+                                                   body: @project_for_hostname_response,
                                                    headers: { 'Content-Type' => 'application/json' } },
                                                  { status: 200,
-                                                   body: sla_levels_response,
+                                                   body: @sla_for_hostname_response,
                                                    headers: { 'Content-Type' => 'application/json' } })
     expect(@puppetdb_reporter.generate_line_of_content('sul-frda-prod.stanford.edu')).to eql(['sul-frda-prod.stanford.edu',
                                                                                                 'dlss','foo','foo',
